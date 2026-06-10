b3e4232a482508d198e2d5d1f384c0d929fd1b11
Akshay Rao <akshrb@amazon.com>
2026-06-09 22:18:32 -0700

    init: initial commit, setup repo

---
+++ leetcode/189-rotate-array/189_rotate_array.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <assert.h>
+
+/*
+ * Rotates a given array by k spots to the right
+ * Input  =  [1, 2, 3, 4, 5, 6, 7] k = 3
+ * Output =  [5, 6, 7, 1, 2, 3, 4]
+ *
+ * Input =  [1, 2, 3, 4, 5, 6, 7, 8, 10] k = 2
+ * Output = [8, 10, 1, 2, 3, 4, 5, 6, 7]
+ *
+ * Input  =  [4, 10, 20, -100] k = 1
+ * Output =  [-100, 4, 10, 20]
+ * 
+ * Input  = [4, 10, 20, -100] k = 6
+ * Output = [20, -100, 4, 10]
+*/
+
+static void rotate(int* nums, int numSize, int k);
+static void reverse(int* nums, int i, int j);
+static void swap(int* nums, int i, int j);
+static void assert_eq(int* actual, int* expected, int len);
+
+int main() {
+    /* case 1*/
+    int len_1 = 7;
+    int a_1[] = {1, 2, 3, 4, 5, 6, 7};
+
+    rotate(a_1, len_1, 3);
+    int e_1[] = {5, 6, 7, 1, 2, 3, 4};
+    assert_eq(a_1, e_1, len_1);
+
+
+    rotate(a_1, len_1, 6);
+    int e_2[] = {2, 3, 4, 5, 6, 7, 1};
+    assert_eq(a_1, e_2, len_1);
+
+
+    rotate(a_1, len_1, 35);
+    assert_eq(a_1, a_1, len_1);
+}
+
+void rotate(int* nums, int numSize, int k) {
+    k = k % numSize;
+    int first = 0;
+    int last = numSize - 1;
+    int reverse_point = last - k;
+
+    reverse(nums, first, reverse_point);
+    reverse(nums, reverse_point + 1, last);
+    reverse(nums, first, last);
+}
+
+void reverse(int* nums, int i, int j) {
+    while (i < j) {
+        swap(nums, i++, j--);
+    }
+}
+
+void swap(int* nums, int i, int j) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp; 
+}
+
+void assert_eq(int* actual, int* expected, int len) {
+    for (int i = 0; i < len; i++) {
+        assert(actual[i] == expected[i]);
+    }
+}
---
+++ leetcode/3120-count-special-characters/3120-count-special-characters.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/*
+* Returns count of special characters
+* A special character is defined as found as uppercase and lowercase in word
+* 
+* Input  = "aaAbcBC"
+* Output = 3 (a and A, b and B, c and C)
+*
+* Input  = "abc"
+* Output = 0 
+*
+* Input  = abBCab
+* Output = 1 (b and B)
+*/
+
+static int number_of_special_chars(char* word);
+static bool does_complement_exist(char letter);
+
+int main() {
+
+
+
+}
+
+
+int number_of_special_chars(char* word) {
+
+
+}
+
+bool does_complement_exist(char letter) {
+    int num = letter;
+
+
+}
+
+
+/*
+* Traverse once, fill up a char array. Then check later for each lower case, did
+* we find an upper case. If yes
+*
+* 
+*/
