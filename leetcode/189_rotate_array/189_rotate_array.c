3e5680c25e9d3d503e7b379f69692e61f05f6c7b
Akshay <akshayrao196@gmail.com>
2026-09-07 22:39:59 +0800

    fix: removed in-place mutation, code assered

--- leetcode/189_rotate_array/189_rotate_array.c
+++ leetcode/189_rotate_array/189_rotate_array.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
 /*
  * Rotates a given array by k spots to the right
  * Input  =  [1, 2, 3, 4, 5, 6, 7] k = 3
  * Output =  [5, 6, 7, 1, 2, 3, 4]
- *
+ * [2] [3, 4, 5, 6, 7, 1] 
  * Input =  [1, 2, 3, 4, 5, 6, 7, 8, 10] k = 2
  * Output = [8, 10, 1, 2, 3, 4, 5, 6, 7]
  *
@@ -22,20 +23,25 @@ static void swap(int* nums, int i, int j);
 static void assert_eq(int* actual, int* expected, int len);
 
 int main() {
-    /* case 1*/
-    int len_1 = 7;
-    int a_1[] = {1, 2, 3, 4, 5, 6, 7};
 
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int len_1 = 7;
+
+    /* case 1 */
+    int a_1[len_1];
+    memcpy(a_1, arr, sizeof(arr));
     rotate(a_1, len_1, 3);
     int e_1[] = {5, 6, 7, 1, 2, 3, 4};
     assert_eq(a_1, e_1, len_1);
-
-
+	
+    /* case 2 */
+    memcpy(a_1, arr, sizeof(arr));
     rotate(a_1, len_1, 6);
     int e_2[] = {2, 3, 4, 5, 6, 7, 1};
     assert_eq(a_1, e_2, len_1);
 
-
+    /* case 3 */
+    memcpy(a_1, arr, sizeof(arr));
     rotate(a_1, len_1, 35);
     assert_eq(a_1, a_1, len_1);
 }
